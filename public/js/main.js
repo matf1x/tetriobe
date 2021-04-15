ff7264ed8d749c82fda015556ad4c37c8ffd0b49
Fabian Tack <fabian@matfix.be>
2021-04-15 11:39:55 +0200

    UI fixes
    Fixed some UI elements for mobile & text corrections

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 // Get the navigation
 const navigation = document.getElementById('navigation');
+const mobileMenu = document.getElementsByClassName('mobile-menu-item');
 
 // Listen for scrolling on the page
 window.addEventListener('scroll', () => {
@@ -10,9 +11,16 @@ window.addEventListener('scroll', () => {
     }
 
     // If we scrolled 75 or more, add the minimize class
-    if(scrollObject.y >= 75) {
+    if(scrollObject.y >= 150) {
         navigation.classList.add('minimize');
     } else {
         navigation.classList.remove('minimize');
     }
 })
+
+// Open the mobile menu
+const openMenu = () => {
+    for(i = 0; i < mobileMenu.length; i++) {
+        mobileMenu[i].classList.toggle('visible');
+    }
+}
